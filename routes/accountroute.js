6553a0ec9dcce817471c3484317c023c4f5eeb1e
ReaganOtema <reaganotema2022@gmail.com>
2025-12-04 18:55:36 +0300

    initial fixes

--- routes/accountroute.js
+++ routes/accountroute.js
@@ -1,20 +1,47 @@
 // routes/accountRoutes.js
 const express = require('express');
 const router = express.Router();
+const accountModel = require('../models/accountmodel'); // your model
+const { requireAuth } = require('../middleware/authmiddleware'); // optional auth
 
-// Account home
-router.get('/', (req, res) => {
-  res.send('Account Management Home');
+// Get account management page
+router.get('/', requireAuth, async (req, res) => {
+  try {
+    const accountId = req.user?.account_id; // make sure req.user is set in auth middleware
+    const account = await accountModel.getAccountById(accountId);
+    res.render('account/manage', { account });
+  } catch (err) {
+    console.error('Error fetching account:', err);
+    res.status(500).send('Error fetching account');
+  }
 });
 
 // Update account info
-router.post('/update', (req, res) => {
-  res.send('Account info updated!');
+router.post('/update', requireAuth, async (req, res) => {
+  try {
+    const { firstname, lastname, email } = req.body;
+    const accountId = req.user?.account_id;
+    await accountModel.updateAccountInfo(accountId, firstname, lastname, email);
+    res.send('Account info updated!');
+  } catch (err) {
+    console.error('Error updating account info:', err);
+    res.status(500).send('Error updating account info');
+  }
 });
 
 // Update password
-router.post('/update-password', (req, res) => {
-  res.send('Password updated!');
+router.post('/update-password', requireAuth, async (req, res) => {
+  try {
+    const { password } = req.body;
+    const bcrypt = require('bcrypt');
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const accountId = req.user?.account_id;
+    await accountModel.updatePassword(accountId, hashedPassword);
+    res.send('Password updated!');
+  } catch (err) {
+    console.error('Error updating password:', err);
+    res.status(500).send('Error updating password');
+  }
 });
 
 module.exports = router;
