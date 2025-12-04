852ed2de2d3fd96989c40fb681aa3103796b835e
ReaganOtema <reaganotema2022@gmail.com>
2025-12-04 19:12:03 +0300

    updates

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-const accountroutes = require('./routes/accountroutes');
-const inventoryroutes = require('./routes/inventoryroutes');
+const accountRoutes = require('./routes/accountroutes');
+const inventoryRoutes = require('./routes/inventoryroutes');
 
 const app = express();
 
@@ -16,11 +16,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 // Routes
-app.use('/account', accountroutes);
-app.use('/inventory', inventoryroutes);
+app.use('/account', accountRoutes);
+app.use('/inventory', inventoryRoutes);
 
 // Home
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => {
+  const jwt = require('jsonwebtoken');
+  let user = null;
+  const token = req.cookies.jwt;
+  if (token) {
+    try {
+      user = jwt.verify(token, process.env.JWT_SECRET);
+    } catch {}
+  }
+  res.render('home', { user });
+});
 
 // Logout
 app.get('/logout', (req, res) => {
